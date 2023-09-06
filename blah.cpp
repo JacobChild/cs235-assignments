ba0383610180bdd1eae384254e67596de78b9576
Jacob Child <childjacob2@gmail.com>
2023-09-06 15:42:35 -0700

    re doing c++ yay

---
+++ blah.cpp
@@ -0,0 +1,10 @@
+//includes
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    //first try, for a hello world
+    string hello = "Hello World!";
+    cout << hello << endl;
+}
